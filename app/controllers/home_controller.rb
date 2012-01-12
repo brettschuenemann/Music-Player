aefd70b543a948dd90a0e479b748a7983f0f9de0
tropicana411 <brettschuenemann@gmail.com>
2012-01-11 23:17:20 -0800

    testinttt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,11 @@ class HomeController < ApplicationController
   	song = params[:q]
  	client = YouTubeIt::Client.new
     cl = client.videos_by(:include => :most_viewed, :query=>song)
-    songid = cl.videos[0].player_url
-    title = cl.videos[0].title
-    puts title
-    puts songid
+    url = cl.videos[0].player_url.split('=')[1]
+    @songid = url.split('&')[0]
+    @title = cl.videos[0].title
+    @s = Song.new(:name => @title, :songid => @songid)
+    @s.save
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Ytaudioplayer::Application.routes.draw do
+  get "play/home"
+
   get "home/index"
 
   resources :songs
@@ -53,7 +55,6 @@ Ytaudioplayer::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'home#index'
-
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
